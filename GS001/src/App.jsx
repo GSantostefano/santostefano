fbd439d9b2b7dc980c4574d5a897323c2c7914f5
GSantostefano <gabrielsantostefano@gmail.com>
2024-05-12 14:25:08 -0300

    0003
    0003

--- GS001/src/App.jsx
+++ GS001/src/App.jsx
@@ -1,9 +1,10 @@
 
 import './App.css'
-
+import { FcNext } from "react-icons/fc";
 function App() {
   return (
     <>
+    <div><FcNext /></div>
       <h1>Gabriel Santostefano</h1>
       <div className="card">
         
