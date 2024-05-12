cedecc8d8bf741d7471026ab1c7647847d4a8408
GSantostefano <gabrielsantostefano@gmail.com>
2024-05-12 17:49:53 -0300

    1
    1

--- GS001/src/App.jsx
+++ GS001/src/App.jsx
@@ -4,17 +4,8 @@ import { FcNext } from "react-icons/fc";
 function App() {
   return (
     <>
-    <div><FcNext /></div>
-      <h1>Gabriel Santostefano</h1>
-      <div className="card">
-        
-        <p>
-          Developer 
-        </p>
-      </div>
-      <p className="read-the-docs">
-      Este es la primera pagina de muchas 
-      </p>
+
+
     </>
   )
 }
