8ea8fd53ab2feafce85edda35911362ba8486980
GSantostefano <gabrielsantostefano@gmail.com>
2024-07-06 11:43:29 -0300

    2
    2

--- GS001/src/Components/Etapas/Flop.jsx
+++ GS001/src/Components/Etapas/Flop.jsx
@@ -1,7 +1,7 @@
-import PokerRangeSelector from '../../Components/PokerRangeSelector/PokerRangeSelector';
+
 const Flop = () => {
     return <div>Contenido de FLOP
-      <PokerRangeSelector/>
+      
     </div>;
   };
   
