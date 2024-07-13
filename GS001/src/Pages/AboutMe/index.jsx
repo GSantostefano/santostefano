a3a5cee2a523f8b73ce7be22633492799cf52d8b
GSantostefano <gabrielsantostefano@gmail.com>
2024-07-13 15:04:25 -0300

    1
    1

--- GS001/src/Pages/AboutMe/index.jsx
+++ GS001/src/Pages/AboutMe/index.jsx
@@ -1,43 +1,78 @@
-
 import Layout from '../../Components/Layout';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faGithub, faLinkedin, faInstagram, faWhatsapp } from '@fortawesome/free-brands-svg-icons';
+import { faHtml5, faCss3Alt, faJs, faReact, faGit, faNodeJs } from '@fortawesome/free-brands-svg-icons'; // íconos de marcas
+import { faDatabase, faCode, faServer } from '@fortawesome/free-solid-svg-icons'; // íconos sólidos
 
 function AboutMe() {
   return (
     <Layout>
-      <div>
-        <h1>About Me</h1>
+      <div className='w-9/12'>
+        <h1 className='text-red-600 text-lg'>About Me</h1>
         <section>
-          <h2>Background</h2>
-          <p>
-            Hello! 👋🏻 I'm Gabriel, and I have a background in information systems engineering and business administration.
-          </p>
+          <p>Hello! 👋🏻 I'm Gabriel, and I have a background in information systems engineering and business administration.</p>
         </section>
+
         <section>
-          <h2>Professional Growth</h2>
+          <h2 className='text-red-600 mt-5 text-lg'>Professional Growth</h2>
           <p>
             I'm passionate about personal and professional growth, with a strong focus on teamwork. In 2023, I made a significant decision to reinvent myself professionally by diving into an area I've always been enthusiastic about - programming.
           </p>
         </section>
+
         <section>
-          <h2>Web Development Skills</h2>
-          <ul>
-            <li>HTML</li>
-            <li>CSS</li>
-            <li>JavaScript</li>
-            <li>Git</li>
-            <li>NodeJS</li>
-            <li>Express</li>
-            <li>React</li>
-            <li>Redux</li>
-            <li>SQL</li>
-            <li>Sequelize</li>
+          <h2 className='mt-5 text-red-600 text-lg'>Web Development Skills</h2>
+          <p>As part of my journey in web development, I have honed my skills in various technologies, including:</p>
+          <ul className="mt-5 flex space-x-4 mb-4">
+            <li >HTML <FontAwesomeIcon icon={faHtml5} /></li>
+            <li>CSS <FontAwesomeIcon icon={faCss3Alt} /></li>
+            <li>JavaScript <FontAwesomeIcon icon={faJs} /></li>
+            <li>Git <FontAwesomeIcon icon={faGit} /></li>
+            <li>NodeJS <FontAwesomeIcon icon={faNodeJs} /></li>
+            <li>Express <FontAwesomeIcon icon={faServer} /></li>
+            <li>React <FontAwesomeIcon icon={faReact} /></li>
+            <li>Redux <FontAwesomeIcon icon={faCode} /></li> {/* Aquí se usa faCode */}
+            <li>SQL <FontAwesomeIcon icon={faDatabase} /></li> {/* Aquí se usa faDatabase */}
+            <li>Sequelize <FontAwesomeIcon icon={faDatabase} /></li> {/* Aquí se usa faDatabase */}
           </ul>
         </section>
+
         <section>
-          <h2>Passion for Web Application Development</h2>
+          <h2 className='text-red-600  mt-5 text-lg'>Passion for Web Application Development</h2>
           <p>
-            I am passionate about web application development and continually strive to improve my skills in this area.
+            I'm truly passionate about web application development, with a keen focus on creating innovative and scalable solutions to tackle real-world challenges. I have a problem-solving mindset and a strong work ethic to drive projects forward.
           </p>
+        </section>
+
+        <section>
+          <h2 className='text-red-600 mt-5 text-lg'>Business Administration Experience</h2>
+          <p>
+            My background in business administration is an asset, enabling me to contribute a comprehensive understanding of production processes and a range of experiences, including:
+          </p>
+          <ul>
+            <li>Customer service (both internal and external)</li>
+            <li>Business creation</li>
+            <li>Project development</li>
+            <li>Accounting processes</li>
+          </ul>
+        </section>
+
+        <section>
+          <p className='text-lime-500  mt-5  text-lg'>Feel free to connect with me, and let's explore opportunities for collaboration and growth!</p>
+          <div className="flex space-x-4 mb-4">
+            <a href="https://github.com/GSantostefano" target="_blank" rel="noopener noreferrer" className="text-white">
+              <FontAwesomeIcon icon={faGithub} size="2x" />
+            </a>
+            <a href="https://www.linkedin.com/in/gabriel-santostefano/" target="_blank" rel="noopener noreferrer" className="text-white">
+              <FontAwesomeIcon icon={faLinkedin} size="2x" />
+            </a>
+            <a href="https://www.instagram.com/gacho.santoss/" target="_blank" rel="noopener noreferrer" className="text-white">
+              <FontAwesomeIcon icon={faInstagram} size="2x" />
+            </a>
+            <a href="https://wa.me/5493435066052" target="_blank" rel="noopener noreferrer" className="text-white">
+              <FontAwesomeIcon icon={faWhatsapp} size="2x" />
+            </a>
+          </div>
         </section>
       </div>
     </Layout>
--- GS001/src/Pages/Contacto/index.jsx
+++ GS001/src/Pages/Contacto/index.jsx
@@ -1,15 +1,71 @@
-
-import Layout from '../../Components/Layout'
+import { useState } from 'react';
+import Layout from '../../Components/Layout';
 
 function Contacto() {
+  const [formData, setFormData] = useState({
+    name: '',
+    email: '',
+    message: ''
+  });
+
+  const handleInputChange = (e) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      const response = await fetch('/send-email', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(formData)
+      });
+
+      if (response.ok) {
+        alert('Correo electrónico enviado correctamente');
+        setFormData({ name: '', email: '', message: '' });
+      } else {
+        throw new Error('Error al enviar el correo electrónico');
+      }
+    } catch (error) {
+      console.error('Error:', error);
+      alert('Error al enviar el correo electrónico');
+    }
+  };
+
   return (
-<div>
-    <Layout>
-      BTC Donate!
+    <Layout >
+      <div className="flex justify-center items-center h-full " >
+      <div className=" min-w-96 max-w-7xl mx-auto px-4 py-8 ">
+        <h1 className="text-3xl font-bold mb-6">Contacto</h1>
+        <form onSubmit={handleSubmit} className="space-y-4">
+          <div>
+            <label htmlFor="name" className="block text-sm font-medium text-red-700">Nombre:</label>
+            <input type="text" id="name" name="name" value={formData.name} onChange={handleInputChange} required
+              className="mt-1 block w-full text-black border-red-300 rounded-md shadow-sm focus:ring-red-500 focus:border-red-500 sm:text-sm" />
+          </div>
+          <div>
+            <label htmlFor="email" className="block text-sm font-medium text-red-700">Correo Electrónico:</label>
+            <input type="email" id="email" name="email" value={formData.email} onChange={handleInputChange} required
+              className="mt-1 block w-full text-black border-red-300 rounded-md shadow-sm focus:ring-red-500 focus:border-red-500 sm:text-sm" />
+          </div>
+          <div>
+            <label htmlFor="message" className="block text-sm font-medium text-red-700">Mensaje:</label>
+            <textarea id="message" name="message" value={formData.message} onChange={handleInputChange} required
+              className="mt-1 block w-full border-red-300 text-black rounded-md shadow-sm focus:ring-red-500 focus:border-red-500 sm:text-sm" />
+          </div>
+          <button type="submit"
+            className="inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-red-600 hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500">
+            Enviar
+          </button>
+        </form>
+      </div>
+      </div>
     </Layout>
-    
-    </div>
-  )
+  );
 }
 
-export default Contacto
+export default Contacto;
