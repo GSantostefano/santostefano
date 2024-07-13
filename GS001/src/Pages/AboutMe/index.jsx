b8bd7ad6404de61c60c1cbefbf7a6e014a63da29
GSantostefano <gabrielsantostefano@gmail.com>
2024-07-13 14:25:44 -0300

    1
    1

--- GS001/src/Pages/AboutMe/index.jsx
+++ GS001/src/Pages/AboutMe/index.jsx
@@ -1,11 +1,47 @@
-import Layout from '../../Components/Layout'
+import React from 'react';
+import Layout from '../../Components/Layout';
 
 function AboutMe() {
   return (
     <Layout>
-      AboutMe
+      <div>
+        <h1>About Me</h1>
+        <section>
+          <h2>Background</h2>
+          <p>
+            Hello! 👋🏻 I'm Gabriel, and I have a background in information systems engineering and business administration.
+          </p>
+        </section>
+        <section>
+          <h2>Professional Growth</h2>
+          <p>
+            I'm passionate about personal and professional growth, with a strong focus on teamwork. In 2023, I made a significant decision to reinvent myself professionally by diving into an area I've always been enthusiastic about - programming.
+          </p>
+        </section>
+        <section>
+          <h2>Web Development Skills</h2>
+          <ul>
+            <li>HTML</li>
+            <li>CSS</li>
+            <li>JavaScript</li>
+            <li>Git</li>
+            <li>NodeJS</li>
+            <li>Express</li>
+            <li>React</li>
+            <li>Redux</li>
+            <li>SQL</li>
+            <li>Sequelize</li>
+          </ul>
+        </section>
+        <section>
+          <h2>Passion for Web Application Development</h2>
+          <p>
+            I am passionate about web application development and continually strive to improve my skills in this area.
+          </p>
+        </section>
+      </div>
     </Layout>
-  )
+  );
 }
 
-export default AboutMe
+export default AboutMe;
