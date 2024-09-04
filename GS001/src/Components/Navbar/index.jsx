e0dc802f826f00412e10d5f3b88b34f3e402d9d4
GSantostefano <gabrielsantostefano@gmail.com>
2024-09-04 16:14:19 -0300

    1
    1

--- GS001/src/Components/Navbar/index.jsx
+++ GS001/src/Components/Navbar/index.jsx
@@ -1,6 +1,10 @@
+import { useState } from "react";
 import { NavLink } from "react-router-dom";
+import { FiMenu } from "react-icons/fi"; // Icono de menú hamburguesa
+import { FiX } from "react-icons/fi"; // Icono de cerrar
 
 const NavBar = () => {
+  const [menuOpen, setMenuOpen] = useState(false); // Estado para manejar el menú móvil
   const activeStyle = "text-yellow-600 underline underline-offset-4";
 
   return (
@@ -11,11 +15,10 @@ const NavBar = () => {
         filter: "drop-shadow(2px 2px 2px rgba(0, 0, 0, 0.1))",
       }}
     >
-      <ul className="flex items-center gap-3">
-        <li className="">
+      <div className="flex justify-between items-center w-full lg:w-auto">
+        <div className="text-4xl flex flex-col items-center">
           <NavLink to="/">
             <div
-              className="text-4xl flex flex-col items-center"
               style={{
                 fontFamily: "'Playfair Display', serif", // Aquí se aplica la fuente
                 filter: "drop-shadow(2px 2px 2px rgba(0, 0, 0, 0.1))",
@@ -25,22 +28,25 @@ const NavBar = () => {
               <span className="-mt-5 text-[8px] font-light">Aromas Únicos</span>
             </div>
           </NavLink>
-        </li>
+        </div>
+        <button
+          className="lg:hidden text-2xl"
+          onClick={() => setMenuOpen(!menuOpen)}
+        >
+          {menuOpen ? <FiX /> : <FiMenu />} {/* Icono cambia cuando el menú está abierto */}
+        </button>
+      </div>
 
-        {/* <li>
-          <NavLink
-            to="/aboutme"
-            className={({ isActive }) => 
-                `relative transition-colors duration-300 transform transition-transform ${isActive ? activeStyle : "hover:text-yellow-600 hover:scale-105 hover:after:scale-x-100 after:scale-x-0 after:transition-transform after:duration-300 after:ease-in-out after:absolute after:bottom-0 after:left-0 after:w-full after:h-0.5 after:bg-yellow-600"}`}
-            >
-            Quienes Somos
-          </NavLink>
-        </li> */}
+      <ul
+        className={`lg:flex items-center gap-3 transition-transform ${
+          menuOpen ? "block" : "hidden"
+        } lg:block`}
+      >
         <li>
           <NavLink
             to="/jabones"
             className={({ isActive }) =>
-              `relative transition-colors duration-300 transform transition-transform ${
+              `relative transition-colors duration-300 transform ${
                 isActive
                   ? activeStyle
                   : "hover:text-yellow-600 hover:scale-105 hover:after:scale-x-100 after:scale-x-0 after:transition-transform after:duration-300 after:ease-in-out after:absolute after:bottom-0 after:left-0 after:w-full after:h-0.5 after:bg-yellow-600"
@@ -54,7 +60,7 @@ const NavBar = () => {
           <NavLink
             to="/velas"
             className={({ isActive }) =>
-              `relative transition-colors duration-300 transform transition-transform ${
+              `relative transition-colors duration-300 transform ${
                 isActive
                   ? activeStyle
                   : "hover:text-yellow-600 hover:scale-105 hover:after:scale-x-100 after:scale-x-0 after:transition-transform after:duration-300 after:ease-in-out after:absolute after:bottom-0 after:left-0 after:w-full after:h-0.5 after:bg-yellow-600"
@@ -68,7 +74,7 @@ const NavBar = () => {
           <NavLink
             to="/sales-de-baño"
             className={({ isActive }) =>
-              `relative transition-colors duration-300 transform transition-transform ${
+              `relative transition-colors duration-300 transform ${
                 isActive
                   ? activeStyle
                   : "hover:text-yellow-600 hover:scale-105 hover:after:scale-x-100 after:scale-x-0 after:transition-transform after:duration-300 after:ease-in-out after:absolute after:bottom-0 after:left-0 after:w-full after:h-0.5 after:bg-yellow-600"
@@ -82,7 +88,7 @@ const NavBar = () => {
           <NavLink
             to="/deco-concreto"
             className={({ isActive }) =>
-              `relative transition-colors duration-300 transform transition-transform ${
+              `relative transition-colors duration-300 transform ${
                 isActive
                   ? activeStyle
                   : "hover:text-yellow-600 hover:scale-105 hover:after:scale-x-100 after:scale-x-0 after:transition-transform after:duration-300 after:ease-in-out after:absolute after:bottom-0 after:left-0 after:w-full after:h-0.5 after:bg-yellow-600"
@@ -96,7 +102,7 @@ const NavBar = () => {
           <NavLink
             to="/promociones"
             className={({ isActive }) =>
-              `relative transition-colors duration-300 transform transition-transform ${
+              `relative transition-colors duration-300 transform ${
                 isActive
                   ? activeStyle
                   : "hover:text-yellow-600 hover:scale-105 hover:after:scale-x-100 after:scale-x-0 after:transition-transform after:duration-300 after:ease-in-out after:absolute after:bottom-0 after:left-0 after:w-full after:h-0.5 after:bg-yellow-600"
@@ -108,12 +114,16 @@ const NavBar = () => {
         </li>
       </ul>
 
-      <ul className="flex items-center gap-3 ">
+      <ul
+        className={`lg:flex items-center gap-3 transition-transform ${
+          menuOpen ? "block" : "hidden"
+        } lg:block`}
+      >
         <li>
           <NavLink
             to="/preguntas"
             className={({ isActive }) =>
-              `relative transition-colors duration-300 transform transition-transform ${
+              `relative transition-colors duration-300 transform ${
                 isActive
                   ? activeStyle
                   : "hover:text-yellow-600 hover:scale-105 hover:after:scale-x-100 after:scale-x-0 after:transition-transform after:duration-300 after:ease-in-out after:absolute after:bottom-0 after:left-0 after:w-full after:h-0.5 after:bg-yellow-600"
@@ -127,7 +137,7 @@ const NavBar = () => {
           <NavLink
             to="/contacto"
             className={({ isActive }) =>
-              `relative transition-colors duration-300 transform transition-transform ${
+              `relative transition-colors duration-300 transform ${
                 isActive
                   ? activeStyle
                   : "hover:text-yellow-600 hover:scale-105 hover:after:scale-x-100 after:scale-x-0 after:transition-transform after:duration-300 after:ease-in-out after:absolute after:bottom-0 after:left-0 after:w-full after:h-0.5 after:bg-yellow-600"
@@ -141,7 +151,7 @@ const NavBar = () => {
           <NavLink
             to="/perfil"
             className={({ isActive }) =>
-              `relative  duration-50 transform transition-transform ${
+              `relative duration-50 transform ${
                 isActive
                   ? activeStyle
                   : "hover:text-yellow-600 hover:scale-105 hover:after:scale-x-100 after:scale-x-0 after:transition-transform after:duration-300 after:ease-in-out after:absolute after:bottom-0 after:left-0 after:w-full after:h-0.5 after:bg-yellow-600"
@@ -155,4 +165,5 @@ const NavBar = () => {
     </nav>
   );
 };
+
 export default NavBar;
